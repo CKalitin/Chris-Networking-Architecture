31c42f32e9173c8227f018a43ad4b9400a3be17d
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2021-12-04 13:06:24 -0800

    Added Client Networking Package

---
+++ ChrisNetworkingArchitecture/Runtime/Networking/ClientHandle.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class ClientHandle : MonoBehaviour {
+    [HideInInspector] public static bool welcomeReceived;
+
+    public static void Welcome(Packet _packet) {
+        welcomeReceived = true;
+
+        string _msg = _packet.ReadString();
+        int _myId = _packet.ReadInt();
+
+        Debug.Log($"Message from Server: {_msg}");
+        Client.instance.myId = _myId;
+        ClientSend.WelcomeReceived();
+
+        Client.instance.udp.Connect(((IPEndPoint)Client.instance.tcp.socket.Client.LocalEndPoint).Port);
+    }
+
+    #region Client Object
+
+    public static void ClientObjectUpdate(Packet _packet) {
+        int _objectId = _packet.ReadInt();
+        Vector2 _pos = _packet.ReadVector3();
+        Quaternion _rot = _packet.ReadQuaternion();
+        Vector3 _scale = _packet.ReadVector3();
+
+        NetworkManager.instance.ClientObjectUpdate(_objectId, _pos, _rot, _scale);
+    }
+
+    public static void ClientObjectNew(Packet _packet) {
+        int _prefabIndex = _packet.ReadInt();
+
+        NetworkManager.instance.ClientObjectNew(_prefabIndex);
+    }
+
+    public static void ClientObjectDelete(Packet _packet) {
+        int _objectId = _packet.ReadInt();
+        NetworkManager.instance.ClientObjectDelete(_objectId);
+    }
+
+    public static void ClientDataPacket(Packet _packet) {
+        int _clientDataPacketId = _packet.ReadInt(); // Read id of this clientDataPacket
+        Client.clientDataPacketHandlers[_clientDataPacketId](_packet); // Send packet to handler of clientDataPacket of this id
+    }
+
+    #endregion
+
+    #region Client Data Packets Handlers
+
+    // Id = 1, Types = Int, 
+    public static void ReadClientDataPacketTest(Packet _packet) {
+        ClientDataPacket clientDataPacket = new ClientDataPacket(0); // Create new ClientDataPacket
+
+        // Create list of variables (This is manual for every client data packet type)
+        List<object> packetVars = new List<object>();
+        packetVars.Add(1); // This adds the id of the clientDataPacket
+        packetVars.Add(_packet.ReadInt());
+
+        // Set ClientDataPacket vars to new list of vars
+        clientDataPacket.Vars = packetVars;
+
+        NetworkManager.instance.ClientDataPacket(clientDataPacket);
+    }
+
+    #endregion
+}
---
+++ ChrisNetworkingArchitecture/Runtime/Networking/ClientSend.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientSend : MonoBehaviour {
+    public static bool singlePlayer = false;
+
+    private static void SendTCPData(Packet _packet) {
+        _packet.WriteLength();
+        if (singlePlayer) {
+
+        } else {
+            if (Client.instance.isConnected && ClientHandle.welcomeReceived) {
+                Client.instance.tcp.SendData(_packet);
+            }
+        }
+    }
+
+    private static void SendUDPData(Packet _packet) {
+        _packet.WriteLength();
+        if (singlePlayer) {
+
+        } else {
+            if (Client.instance.isConnected && ClientHandle.welcomeReceived) {
+                Client.instance.udp.SendData(_packet);
+            }
+        }
+    }
+
+    #region Packets
+
+    public static void WelcomeReceived() {
+        using (Packet _packet = new Packet((int)ClientPackets.welcomeReceived)) { // Using creates a new objects then destroys it
+            _packet.Write(Client.instance.myId);
+            _packet.Write(NetworkManager.instance.Username);
+
+            SendTCPData(_packet);
+        }
+    }
+
+    public static void TCPInput(List<int> _keysDown, List<int> _keysUp) {
+        using (Packet _packet = new Packet((int)ClientPackets.tcpInput)) { // Using creates a new objects then destroys it
+            _packet.Write(_keysDown.Count); // Write length of _keysDown so Server knows how many times to iterate the for loop 
+            _packet.Write(_keysUp.Count); // Write length of _keysUp so Server knows how many times to iterate the for loop
+
+            // Write the ids of the keys down
+            if (_keysDown.Count > 0) {
+                for (int i = 0; i < _keysDown.Count; i++) {
+                    _packet.Write(_keysDown[i]);
+                }
+            }
+
+            // Write the ids of the keys up
+            if (_keysUp.Count > 0) {
+                for (int i = 0; i < _keysUp.Count; i++) {
+                    _packet.Write(_keysUp[i]);
+                }
+            }
+
+            SendTCPData(_packet); // Send Packet
+        }
+    }
+
+    public static void UDPInput(Vector2 _mousePos) {
+        using (Packet _packet = new Packet((int)ClientPackets.udpInput)) { // Using creates a new objects then destroys it
+            _packet.Write(_mousePos);
+
+            SendUDPData(_packet);
+        }
+    }
+
+    public static void ServerDataPacket(ServerDataPacket _serverDataPacket) {
+        using (Packet _packet = new Packet((int)ClientPackets.serverData)) { // Using creates a new objects then destroys it
+            // First value in _clientData is type of the ClientDataPacket, write it to packet
+            _packet.Write((int)_serverDataPacket.Vars[0]);
+
+            //Iterate through vars in _clientData
+            for (int i = 1; i < _serverDataPacket.Vars.Count; i++) {
+                // Check type of Vars[i], if it is the type in the if statement, write data to packet of that type
+                // This is a horrible way to write data, but the solution i came up with, pls fix this future chris
+                if (_serverDataPacket.Vars[i] is byte) {
+                    _packet.Write((Byte)_serverDataPacket.Vars[i]);
+                } else if (_serverDataPacket.Vars[i] is byte[]) {
+                    _packet.Write((Byte[])_serverDataPacket.Vars[i]);
+                } else if (_serverDataPacket.Vars[i] is short) {
+                    _packet.Write((short)_serverDataPacket.Vars[i]);
+                } else if (_serverDataPacket.Vars[i] is int) {
+                    _packet.Write((int)_serverDataPacket.Vars[i]);
+                } else if (_serverDataPacket.Vars[i] is long) {
+                    _packet.Write((long)_serverDataPacket.Vars[i]);
+                } else if (_serverDataPacket.Vars[i] is float) {
+                    _packet.Write((float)_serverDataPacket.Vars[i]);
+                } else if (_serverDataPacket.Vars[i] is bool) {
+                    _packet.Write((bool)_serverDataPacket.Vars[i]);
+                } else if (_serverDataPacket.Vars[i] is string) {
+                    _packet.Write((string)_serverDataPacket.Vars[i]);
+                } else if (_serverDataPacket.Vars[i] is Vector2) {
+                    _packet.Write((Vector2)_serverDataPacket.Vars[i]);
+                } else if (_serverDataPacket.Vars[i] is Vector3) {
+                    _packet.Write((Vector3)_serverDataPacket.Vars[i]);
+                } else if (_serverDataPacket.Vars[i] is Quaternion) {
+                    _packet.Write((Quaternion)_serverDataPacket.Vars[i]);
+                }
+            }
+
+            // Send to srever
+            SendTCPData(_packet);
+        }
+    }
+
+
+    #endregion
+}
